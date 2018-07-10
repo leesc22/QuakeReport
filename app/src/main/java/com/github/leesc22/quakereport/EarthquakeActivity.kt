2de21953b238779c4243127e7e4a020c2462504f
leesc22 <leesiewching22@gmail.com>
2018-07-10 09:22:24 +0800

    Parse JSON response to create earthquakes list

--- app/src/main/java/com/github/leesc22/quakereport/EarthquakeActivity.kt
+++ app/src/main/java/com/github/leesc22/quakereport/EarthquakeActivity.kt
@@ -11,15 +11,8 @@ class EarthquakeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_earthquake)
 
-        // Create a fake list of earthquake locations.
-        val earthquakes = arrayListOf<Earthquake>()
-        earthquakes.add(Earthquake(7.2F,"San Francisco", "Feb 2, 2016"))
-        earthquakes.add(Earthquake(6.1F,"London", "July 20, 2015"))
-        earthquakes.add(Earthquake(3.9F,"Tokyo", "Nov 10, 2014"))
-        earthquakes.add(Earthquake(5.4F,"Mexico City", "May 3, 2014"))
-        earthquakes.add(Earthquake(2.8F,"Moscow", "Jan 31, 2013"))
-        earthquakes.add(Earthquake(4.9F,"Rio de Janeiro", "Aug 19, 2012"))
-        earthquakes.add(Earthquake(1.6F,"Paris", "Oct 30, 2011"))
+        // Get the list of earthquakes from {@link QueryUtils}
+        val earthquakes = QueryUtils.extractEarthquakes()
 
         // Find a reference to the {@link ListView} in the layout
         val earthquakeListView = findViewById<ListView>(R.id.list)
